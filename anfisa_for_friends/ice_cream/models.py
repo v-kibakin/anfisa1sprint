0e039116a0ea432dd47ef10b04c5749eee470520
Виктор Кибакин <v-kibakin@yandex.ru>
2025-10-17 17:12:20 +0300

    replace repeats with base model

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -1,30 +1,27 @@
 from django.db import models
+from core.models import PublishedModel
 
 
 # Категории.
-class Category(models.Model):
-    is_published = models.BooleanField(default=True)
+class Category(PublishedModel):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
     output_order = models.PositiveSmallIntegerField(default=100)
 
 
 # Топпинги.
-class Topping(models.Model):
-    is_published = models.BooleanField(default=True)
+class Topping(PublishedModel):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
 
 
 # Обёртки.
-class Wrapper(models.Model):
-    is_published = models.BooleanField(default=True)
+class Wrapper(PublishedModel):
     title = models.CharField(max_length=256)
 
 
 # Сорта мороженого.
-class IceCream(models.Model):
-    is_published = models.BooleanField(default=True)
+class IceCream(PublishedModel):
     is_on_main = models.BooleanField(default=False)
     title = models.CharField(max_length=256)
     description = models.TextField()
@@ -37,7 +34,8 @@ class IceCream(models.Model):
     )
     category = models.ForeignKey(
         Category,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='ice_cream'
     )
     toppings = models.ManyToManyField(
         Topping
