dbb1cff8bdeb015a581d38cdb4f2313e055f9068
Виктор Кибакин <v-kibakin@yandex.ru>
2025-10-17 17:04:14 +0300

    add base model

--- anfisa_for_friends/core/models.py
+++ anfisa_for_friends/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class PublishedModel(models.Model):
+    is_published = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
